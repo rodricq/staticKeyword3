16d834305502abfff54447087a4c0470b86dfea3
beverley-dick <beverleydick11@gmail.com>
2023-05-08 17:22:57 +0300

    Add project files.

---
+++ staticKeyword3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace staticKeyword3
+{
+    class Test
+    {
+        public void display1()
+        {
+            Console.WriteLine("Non static method");
+        }
+        public static void display2()
+        {
+            Console.WriteLine("Static method");
+        }
+    }
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Test t1 = new Test();
+            t1.display1();
+            Test.display2();
+            Console.ReadLine();
+        }
+    }
+}
